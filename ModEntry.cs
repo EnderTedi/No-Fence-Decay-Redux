032b7ba1d3bf24d03742014e3428ba9faaece7fa
EnderTedi <endertedi2@gmail.com>
2024-03-24 12:20:03 +0100

    Add comments

--- ModEntry.cs
+++ ModEntry.cs
@@ -15,15 +15,23 @@ namespace NoFenceDecayRedux
     {
         public override void Entry(IModHelper helper)
         {
+            // Init Harmony
             var Harmony = new Harmony(this.ModManifest.UniqueID);
+
+            /**********************************
+             * Harmony prefix Fence.minutesElapsed
+             * minutesElapsed applies fence decay
+             **********************************/
             Harmony.Patch(
                 original: AccessTools.Method(typeof(StardewValley.Fence), nameof(StardewValley.Fence.minutesElapsed)),
                 prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.minutesElapsed_Patched))
                 );
         }
 
+        
         private static bool minutesElapsed_Patched(int minutes)
         {
+            // Return false, stopping fence decay from applying to fences
             return false;
         }
     }
